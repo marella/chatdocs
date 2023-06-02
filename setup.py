83d829b4250228e9c1323e3d96ff9bdd29ee98a4
Ravindra Marella <mv.ravindra007@gmail.com>
2023-06-03 02:57:27 +0530

    0.1.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ name = "chatdocs"
 
 setup(
     name=name,
-    version="0.0.0",
+    version="0.1.0",
     description="Chat with your documents offline using AI.",
     long_description=long_description,
     long_description_content_type="text/markdown",
