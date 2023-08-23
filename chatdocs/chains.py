c8b8bba7cbe107b95755eb6b8d29f44f792fb2e8
Matteo Cargnelutti <mcargnelutti@law.harvard.edu>
2023-08-23 16:20:59 -0400

    Allow changing LangChain's default prompt
    Adds a new config option, `retriever.custom_prompt`, allowing to replace LangChain's default prompt with a custom one.
    
    See: https://github.com/langchain-ai/langchain/blob/master/libs/langchain/langchain/chains/question_answering/stuff_prompt.py#L10

--- chatdocs/chains.py
+++ chatdocs/chains.py
@@ -1,6 +1,11 @@
 from typing import Any, Callable, Dict, Optional
 
 from langchain.chains import RetrievalQA
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+    SystemMessagePromptTemplate
+)
 
 from .llms import get_llm
 from .vectorstores import get_vectorstore
@@ -14,8 +19,22 @@ def get_retrieval_qa(
     db = get_vectorstore(config)
     retriever = db.as_retriever(**config["retriever"])
     llm = get_llm(config, callback=callback)
+    chain_type_kwargs = {}
+
+    # Prepare and pass custom prompt if provided
+    if "retriever" in config and "custom_prompt" in config["retriever"]:
+        custom_prompt = config["retriever"]["custom_prompt"]
+
+        messages = [
+            SystemMessagePromptTemplate.from_template(custom_prompt),
+            HumanMessagePromptTemplate.from_template("{question}")
+        ]
+
+        chain_type_kwargs["prompt"] = ChatPromptTemplate.from_messages(messages)
+
     return RetrievalQA.from_chain_type(
         llm=llm,
         retriever=retriever,
         return_source_documents=True,
+        chain_type_kwargs=chain_type_kwargs
     )
